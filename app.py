d37606939a917fc9375f7550f030590b628f3f3e
Noel AI <132602447+noelakaunni@users.noreply.github.com>
2023-07-26 22:47:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ def chat_with_assistant():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+ #   app.run(debug=True)
